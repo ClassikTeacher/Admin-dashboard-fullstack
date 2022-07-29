dc5642a332cf9d90874ce941180b9f76cb1437a8
ClassikTeacher <dvakarata.94@gmail.com>
2022-07-29 22:24:26 +0300

    7 day

--- client/src/components/UI/DepartmentInfo/DepartmentInfo.tsx
+++ client/src/components/UI/DepartmentInfo/DepartmentInfo.tsx
@@ -1,6 +1,7 @@
 import React, { FC } from 'react'
 import moment from 'moment'
 import { IDepartment } from '../../../models/IDepartment'
+import styles from './DepartmentInfo.module.css'
 
 
 interface DepartmentInfoProps{
@@ -9,21 +10,21 @@ interface DepartmentInfoProps{
 
 const DepartmentInfo: FC<DepartmentInfoProps> = ({department})=>{
     return(
-        <div className="department-info">
+        <div className={styles.departmentInfo}>
         <h2>Department</h2>
-        <div className="department-info__item">
+        <div className={styles.departmentInfo__item}>
           <h4>Name</h4>
           <p>{department?.name}</p>
         </div>
-        <div className="department-info__item">
+        <div className={styles.departmentInfo__item}>
           <h4>Date</h4>
           <p>{moment(department?.date).format("yyyy-mm-DD")}</p>
         </div>
-        <div className="department-info__item">
+        <div className={styles.departmentInfo__item}>
           <h4>Description</h4>
           <p>{department?.description}</p>
         </div>
-        <div className="department-info__item">
+        <div className={styles.departmentInfo__item}>
           <h4>Departmnet head</h4>
           <p>{department?.department_head}</p>
         </div>
